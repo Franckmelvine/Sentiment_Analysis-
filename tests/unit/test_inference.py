8100b3147109c8f5e3d464906e71a5879fedc380
OwenDiel <mouketoudielowen@gmail.com>
2025-03-31 17:46:07 +0200

    Update test_inference.py

--- tests/unit/test_inference.py
+++ tests/unit/test_inference.py
@@ -1,19 +1,14 @@
 import unittest
-from unittest.mock import patch
 from src.inference import predict_sentiment
 
 
 class TestInference(unittest.TestCase):
-    @patch("src.inference.predict_sentiment")
-    def test_prediction_output(self, mock_predict):
-        # On simule la sortie du modèle
-        mock_predict.return_value = {
-            "label": "positive",
-            "score": 0.987
-        }
+    def setUp(self):
+        self.model_path = "nlptown/bert-base-multilingual-uncased-sentiment"
 
+    def test_prediction_output(self):
         sample_text = "J'adore ce produit, il est génial !"
-        result = predict_sentiment("fake-model-path", sample_text)
+        result = predict_sentiment(self.model_path, sample_text)
 
         self.assertIn("label", result)
         self.assertIn("score", result)
@@ -21,7 +16,3 @@ class TestInference(unittest.TestCase):
         self.assertIsInstance(result["score"], float)
         self.assertGreaterEqual(result["score"], 0.0)
         self.assertLessEqual(result["score"], 1.0)
-
-
-if __name__ == "__main__":
-    unittest.main()
