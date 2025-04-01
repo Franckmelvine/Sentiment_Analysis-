83b51e4b16af166ca7f985e52ed3298f6dc0ea4b
OwenDiel <mouketoudielowen@gmail.com>
2025-04-01 16:12:25 +0200

    sauvegarde avant rebase

--- src/api.py
+++ src/api.py
@@ -2,18 +2,16 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from src.inference import predict_sentiment
 
-
 app = FastAPI()
-
 
 class TextInput(BaseModel):
     text: str
 
+@app.get("/")
+def read_root():
+    return {"message": "Bienvenue dans l'API de détection de sentiments. Allez sur /docs pour tester."}
 
 @app.post("/predict")
 def predict(input: TextInput):
-    result = predict_sentiment(input.text)
-    return {
-        "sentiment": result[0]['label'],
-        "confidence": result[0]['score']
-    }
+    sentiment = predict_sentiment(input.text)
+    return {"sentiment": sentiment}
